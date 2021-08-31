b43c3e9ff0529571275920a0b1c74a43977b715a
milch4089 <milch4089@gmail.com>
2021-08-31 19:33:58 +0900

    오타수정

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { Client, Intents, Collection} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const { prefix, token } = require("./settings.json");
-client.commands = new C ollection;
+client.commands = new Collection;
 const commandFiles = fs
   .readdirSync("./commands")
   .filter((file) => file.endsWith(".js"));

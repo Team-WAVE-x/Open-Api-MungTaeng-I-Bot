f99db0fdee0a0b5af859897c9771bdade99e54a7
milch4089 <milch4089@gmail.com>
2021-09-02 14:18:06 +0900

    코로나 추가

---
+++ commands/covid19.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require('discord.js');
+const request = require('request');
+const { covid19 } = require("../settings.json");
+
+module.exports = {
+    name: "코로나",
+    execute(message) {
+        var options = {
+            'method': 'GET',
+            'url': encodeURI('https://api.corona-19.kr/korea/?serviceKey=' + covid19),
+            'headers': {
+
+            }
+        };
+        request(options, function (error, response) {
+            if (error) throw new Error(error);
+            let covid19json = JSON.parse(response.body);
+            console.log(JSON.parse(response.body).TotalCase);
+            var Embed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('코로나19 현황')
+                .setColor('#0099ff')
+                .addFields(
+                    {name: '누적 확진자', value: covid19json.TotalCase+'(명)'},
+                    {name: '누적 완치자', value: covid19json.TotalRecovered+'(명)'},
+                    {name: '누적 사망자', value: covid19json.TotalDeath+'(명)'},
+                    {name: '격리, 치료중인 확진자', value: covid19json.NowCase+'(명)'})
+                    .setTimestamp()
+                    .setFooter('MungtengI');
+                return message.channel.send({ embeds: [Embed] });
+        });
+    }
+}
--- commands/help.js
+++ commands/help.js
@@ -8,11 +8,12 @@ module.exports = {
             .setTitle("**Help**")
             .addField(':help', '명령어 목록 및 도움말을 보여줍니다')
             .addField(':맛집 (시/군명)', '"경기도"에 있는 맛집들을 보여줍니다')
+            .addField(':코로나', '코로나19 현황을 알려줍니다')
             .addField(':쇼핑 (검색내용)', '검색내용을 바탕으로 쇼핑정보를 보여줍니다')
             .addField(':고양이', '고양이 사진을 랜덤하게 보여줍니다')
             .addField(':강아지', '강아지 사진을 랜덤하게 보여줍니다')
             .setTimestamp()
-            .setFooter('Mungtengyee');
+            .setFooter('MungtengI');
 
         message.channel.send({ embeds: [exampleEmbed] });
     }
